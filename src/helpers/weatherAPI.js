c0bd74f4b13e3ebed8280c000fc4f8c84329b19d
José Luiz Medeiros Mendes Neto <jlmendesneto@hotmail.com>
2023-08-09 22:45:12 -0300

    começando primeiro requisito

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,7 +1,18 @@
-// Remova os comentários a medida que for implementando as funções
+const TOKEN = import.meta.env.VITE_TOKEN;
 
-export const searchCities = (/* term */) => {
-//   seu código aqui
+export const searchCities = async (cidade) => {
+  const URL = `http://api.weatherapi.com/v1/search.json?lang=pt&key=${TOKEN}&q=${cidade}`;
+  try {
+    const resposta = await fetch(URL);
+    const dados = await resposta.json();
+    if (dados.length === 0) {
+      window.alert('Nenhuma cidade encontrada');
+    }
+    return dados;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
 };
 
 export const getWeatherByCity = (/* cityURL */) => {
